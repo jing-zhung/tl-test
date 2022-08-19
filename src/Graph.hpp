8f884e61a47d6303751fb63304f0c2a440015692
jing-zhung <jing_zhung@hotmail.com>
2022-08-19 12:32:50 +0800

    add graph data structure definition

---
+++ src/Graph.hpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+#include <list>
+
+class Vertex
+{
+    public:
+        std::string data_;
+        Vertex(std::string data)
+        {
+            this->data_ = data;
+        };
+};
+
+class Graph
+{
+    private:
+        std::vector<std::list<Vertex*>*> adj_list;
+    public:
+        void addVertex(Vertex* vertex)
+        {
+            std::list<Vertex*> *current_list = new std::list<Vertex*>;
+            current_list->push_back(vertex);
+            adj_list.push_back(current_list);
+        };
+        void addEdge(int source, int destination)
+        {
+            std::list<Vertex*> *current_list = adj_list[source];
+            Vertex* destination_vertex = adj_list[destination]->front();
+            current_list->push_back(destination_vertex);
+        };
+        bool checkEdge(int source, int destination)
+        {
+            std::list<Vertex*> *current_list = adj_list[source];
+            Vertex* destination_vertex = adj_list[destination]->front();
+            for(auto vertex : *current_list)
+            {
+                if(vertex == destination_vertex)
+                {
+                    return true;
+                }
+            }
+            return false;
+        };
+        void printGraph()
+        {
+            for(auto list : adj_list)
+            {
+                for(auto vertex : *list)
+                {
+                    std::cout << vertex->data_ << " -> ";
+                }
+                std::cout << std::endl;
+            }
+        };
+        void deleteGraph()
+        {
+            for(auto list : adj_list)
+            {
+                delete list;
+            }
+        }
+};
